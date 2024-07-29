b8f87766caa7cd594b0350520a1bc0546dd107c1
Bogdan Petrovici <bogdan.petrovici@groupvery.com>
2024-07-29 17:06:35 +0300

    Added logic for handling multiplication.

--- ExpressionCalculator.Test/ExpressionCalculatorTests.cs
+++ ExpressionCalculator.Test/ExpressionCalculatorTests.cs
@@ -34,6 +34,26 @@ namespace ExpressionCalculator.Test
         }
 
         [Test]
+        public void ExpressionConverter_InputExpressionWithMultiplication_ReturnsPostfixNotation()
+        {
+            var converter = new ExpressionConverter("12+45-5*10+8");
+            var postfixedExpression = converter.ConvertToQueue();
+            Assert.IsNotNull(postfixedExpression);
+            Assert.That(postfixedExpression.Count, Is.EqualTo(9));
+            Assert.That(postfixedExpression.Dequeue, Is.EqualTo("12"));
+            Assert.That(postfixedExpression.Dequeue, Is.EqualTo("45"));
+            Assert.That(postfixedExpression.Dequeue, Is.EqualTo("+"));
+            Assert.That(postfixedExpression.Dequeue, Is.EqualTo("5"));
+            Assert.That(postfixedExpression.Dequeue, Is.EqualTo("10"));
+            Assert.That(postfixedExpression.Dequeue, Is.EqualTo("*"));
+            Assert.That(postfixedExpression.Dequeue, Is.EqualTo("-"));
+            Assert.That(postfixedExpression.Dequeue, Is.EqualTo("8"));
+            Assert.That(postfixedExpression.Dequeue, Is.EqualTo("+"));
+
+            Assert.That(converter.Convert(), Is.EqualTo("12 45 + 5 10 * - 8 + "));
+        }
+
+        [Test]
         public void ExpressionConverter_MissingOperand_ThrowsException()
         {
             var converter = new ExpressionConverter("12+3++");
@@ -53,6 +73,14 @@ namespace ExpressionCalculator.Test
             var converter = new ExpressionConverter("12+45-56+8");
             var result = converter.Compute();
             Assert.That(result, Is.EqualTo(9));
+        }
+
+        [Test]
+        public void ExpressionConverter_InputExpressionWithMultiplication_ComputesExpression()
+        {
+            var converter = new ExpressionConverter("12+45-5*10+8");
+            var result = converter.Compute();
+            Assert.That(result, Is.EqualTo(15));
         }
     }
 }
